41ea3e740bb99c9d59065901960f98d78a39ad8d
bsutherl2 <sutherland.b@husky.neu.edu>
2016-10-25 15:41:14 -0400

    ps-06

---
+++ ps-06/corpse.js
@@ -0,0 +1,74 @@
+var myCanvas = document.getElementById('corpse');
+var drawingPad = myCanvas.getContext('2d');
+var circleDegrees = 2 * Math.PI;
+var y0  = 0;
+var y1 = 240;
+var y2 = 480;
+var height = y3 = 720;
+var x0 = 0;
+var x1 = 125; // left connection
+var x2 = 275; // right connection
+var width = x3 = 400;
+var horizontalCenter = 200; // centerLine
+
+var bsutherl2 = {};
+
+var drawImage = function(filename, ctx, x, y) {
+  var img = document.createElement("img");
+  img.src = filename;
+  img.addEventListener("load", function() {
+    ctx.drawImage(img, x, y);
+  });
+}
+
+bsutherl2.drawGrid = function(ctx) {
+  ctx.beginPath();
+  ctx.moveTo(x1,y0);
+  ctx.lineTo(x1,y3);
+
+  ctx.moveTo(x2,y0);
+  ctx.lineTo(x2,y3);
+
+  ctx.moveTo(x1,y0);
+  ctx.lineTo(x1,y3);
+
+  ctx.moveTo(x0,y1);
+  ctx.lineTo(x3,y1);
+
+  ctx.moveTo(x0,y2);
+  ctx.lineTo(x3,y2);
+  ctx.stroke();
+}
+
+bsutherl2.drawArm = function(ctx, left) {
+
+};
+
+bsutherl2.drawLeg = function(ctx, left) {
+
+};
+
+
+bsutherl2.drawTop = function(ctx) {
+
+  drawImage('head.jpg', ctx, 0, 0);
+};
+
+bsutherl2.drawMiddle = function(ctx) {
+    drawImage('body.jpg', ctx, 0, 240);
+
+
+};
+
+bsutherl2.drawBottom = function(ctx){
+  drawImage('legs.jpg', ctx, 0, 480);
+};
+
+bsutherl2.drawCorpse = function(ctx) {
+  bsutherl2.drawTop(ctx);
+  bsutherl2.drawMiddle(ctx);
+  bsutherl2.drawBottom(ctx);
+  bsutherl2.drawGrid(ctx);
+};
+
+window.bsutherl2 = bsutherl2;
